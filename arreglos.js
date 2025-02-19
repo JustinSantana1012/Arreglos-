e1f58f09d7921ce37c8c908282c1a7a3c663cd89
JustinSantana1012 <Santanard1012@gmail.com>
2025-02-19 09:32:39 -0400

    Add files via upload

--- arreglos.js
+++ arreglos.js
@@ -1,15 +1,15 @@
-let Carros = [];
-Carros.push('Charger', 'Ferrari', 'Pagani', 'Toyota')
-console.log(Carros[3]);
-Carros.unshift('Ferrari');
-console.log(Carros)
-Carros[4] = 'Toyota'
-console.log(Carros);
-Carros.shift(Carros); 
-console.log(Carros)
-Carros.splice(2,1)
-console.log(Carros)
-delete(Carros[2])
-console.log(Carros)
-Carros[2] = 'Ferrari'
-console.log(Carros)
+let Comida = [];
+Comida.push('Mondongo', 'Rulo', 'Guineo', 'Mangu')
+console.log(Comida[3]);
+Comida.unshift('Mondongo');
+console.log(Comida)
+Comida[4] = 'Rulo'
+console.log(Comida);
+Comida.shift(Comida); 
+console.log(Comida)
+Comida.splice(2,1)
+console.log(Comida)
+delete(Comida[2])
+console.log(Comida)
+Comida[2] = 'Rulo'
+console.log(Comida)
